c7812f2cee95439edfdf00f17b3ddb2edc2e8865
airtondias <airtondias2930@gmail.com>
2023-04-05 22:03:38 -0300

    Iniciando com JS

---
+++ script.js
@@ -0,0 +1,23 @@
+// david comentario
+/*
+segundo comentario
+*/
+console.log('olá mundo')
+//prompt("qual sua idade")
+
+
+//conhecendo e trabalhando com variaveis
+const diaDeNascimento = 22;
+let texto = "Meu dia de nascimento eh:"
+console.log(texto)
+//concatenando variaveis
+console.log(texto, diaDeNascimento)
+// exemplos
+const primeiroNome = "Airton"
+const meuSobrenome = "Dias"
+let minhaIdade = 28;
+let souEstudante = true;
+
+//Imprimir irformações no console
+console.log("Meu nome é ", primeiroNome, meuSobrenome, " tenho ", minhaIdade,
+"estou estudando: ", souEstudante)
